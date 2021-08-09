f88bffb68017074a7718a8cef067f48b93d38d73
El-Leto <elleto.life@gmail.com>
2021-08-09 11:10:42 +0300

    Add snippets

---
+++ js/snippets.js
@@ -0,0 +1,8 @@
+const firstTime = document.querySelector('.first-time');
+firstTime.textContent = 'Пн-Пт: 10:00-20:00';
+
+const secondTime = document.querySelector('.second-time');
+secondTime.textContent = 'Сб: 10:00-18:00 Вс: выходной';
+
+const weekendTime = document.querySelector('.weekend-time');
+weekendTime.textContent = '';
