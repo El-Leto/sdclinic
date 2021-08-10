450127436655c0638965bdb4a739ef6711d2288a
El-Leto <elleto.life@gmail.com>
2021-08-10 14:55:27 +0300

    Fix

--- js/snippets.js
+++ js/snippets.js
@@ -1,8 +1,8 @@
-const firstTime = document.querySelector('.first-time');
-firstTime.textContent = 'Пн-Пт: 10:00-20:00';
+const firstTime = document.querySelectorAll('.first-time');
+firstTime.forEach(item => item.textContent = 'Пн-Пт: 10:00-20:00')
 
-const secondTime = document.querySelector('.second-time');
-secondTime.textContent = 'Сб: 10:00-18:00 Вс: выходной';
+const secondTime = document.querySelectorAll('.second-time');
+secondTime.forEach(item => item.textContent = 'Сб: 10:00-18:00 Вс: выходной')
 
-const weekendTime = document.querySelector('.weekend-time');
-weekendTime.textContent = '';
+const weekendTime = document.querySelectorAll('.weekend-time');
+weekendTime.forEach(item => item.textContent = '')
