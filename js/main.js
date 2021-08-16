2aa1fe31a72ff7978af48ec1fdd24dd838dff202
El-Leto <elleto.life@gmail.com>
2021-08-16 20:03:59 +0300

    add carousel-vrachi

--- js/main.js
+++ js/main.js
@@ -84,6 +84,28 @@
 				}
 			}
 		});
+		$('.carousel-vrachi').owlCarousel({
+	    loop:true,
+	    autoplay: true,
+	    margin: 70,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:false,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:2
+	      },
+	      1000:{
+	        items:3
+	      }
+	    }
+		});
 
 	};
 	carousel();
--- js/work-data.js
+++ js/work-data.js
@@ -1,19 +1,28 @@
 let sklyarov = document.querySelector('#vrach-stazh-sklyarov');
+const sklyarovAll = document.querySelectorAll('#vrach-stazh-sklyarov');
 const firstWorkTimeSklyarov = sklyarov.dataset.work;
 
 let pleshivciva = document.querySelector('#vrach-stazh-pleshivciva');
+const pleshivcivaAll = document.querySelectorAll('#vrach-stazh-pleshivciva');
 const firstWorkTimePleshivciva = pleshivciva.dataset.work;
 
 let daladanova = document.querySelector('#vrach-stazh-daladanova');
+const daladanovaAll = document.querySelectorAll('#vrach-stazh-daladanova');
 const firstWorkTimeDaladanova = daladanova.dataset.work;
 
 let mishkina = document.querySelector('#vrach-stazh-mishkina');
+const mishkinaAll = document.querySelectorAll('#vrach-stazh-mishkina');
 const firstWorkTimeMishkina = mishkina.dataset.work;
+
+let kruteleva = document.querySelector('#vrach-stazh-kruteleva');
+const krutelevaAll = document.querySelectorAll('#vrach-stazh-kruteleva');
+const firstWorkTimeKruteleva = kruteleva.dataset.work;
 
 const startWorkDateSklyarov = new Date(firstWorkTimeSklyarov);
 const startWorkDatePleshivciva = new Date(firstWorkTimePleshivciva);
 const startWorkDateDaladanova = new Date(firstWorkTimeDaladanova);
 const startWorkDateMishkina = new Date(firstWorkTimeMishkina);
+const startWorkDateKruteleva = new Date(firstWorkTimeKruteleva);
 
 const dateNow = new Date();
 
@@ -38,7 +47,7 @@ function timeSklyarov(dateNow, startWorkDateSklyarov) {
   days %= 30;
   months %= 12;
 
-  sklyarov.textContent = `Стаж работы ${years} л. ${months} мес.`;
+  sklyarovAll.forEach(item => item.textContent = `Стаж работы ${years} лет ${months} мес.`);
 };
 
 timeSklyarov(dateNow, startWorkDateSklyarov);
@@ -60,7 +69,7 @@ function timePleshivciva(dateNow, startWorkDatePleshivciva) {
   days %= 30;
   months %= 12;
 
-  pleshivciva.textContent = `Стаж работы ${years} г. ${months} мес.`;
+  pleshivcivaAll.forEach(item => item.textContent = `Стаж работы ${years} г. ${months} мес.`);
 };
 
 timePleshivciva(dateNow, startWorkDatePleshivciva);
@@ -82,7 +91,7 @@ function timeDaladanova(dateNow, startWorkDateDaladanova) {
   days %= 30;
   months %= 12;
 
-  daladanova.textContent = `Стаж работы ${years} л. ${months} мес.`;
+  daladanovaAll.forEach(item => item.textContent = `Стаж работы ${years} лет ${months} мес.`);
 };
 
 timeDaladanova(dateNow, startWorkDateDaladanova);
@@ -104,7 +113,29 @@ function timeMishkina(dateNow, startWorkDateMishkina) {
   days %= 30;
   months %= 12;
 
-  mishkina.textContent = `Стаж работы ${years} л. ${months} мес.`;
+  mishkinaAll.forEach(item => item.textContent = `Стаж работы ${years} лет ${months} мес.`);
 };
 
 timeMishkina(dateNow, startWorkDateMishkina);
+
+function timeKruteleva(dateNow, startWorkDateKruteleva) {
+
+  let diffTiemKruteleva = diffDates(dateNow, startWorkDateKruteleva);
+
+    let seconds = Math.floor(diffTiemKruteleva / 1000),
+      minutes = Math.floor(seconds / 60),
+      hours   = Math.floor(minutes / 60),
+      days    = Math.floor(hours / 24),
+      months  = Math.floor(days / 30),
+      years   = Math.floor(days / 365);
+
+  seconds %= 60;
+  minutes %= 60;
+  hours %= 24;
+  days %= 30;
+  months %= 12;
+
+  krutelevaAll.forEach(item => item.textContent = `Стаж работы ${years} лет ${months} мес.`);
+};
+
+timeKruteleva(dateNow, startWorkDateKruteleva);
