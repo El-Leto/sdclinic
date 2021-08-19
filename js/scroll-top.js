8fc320ecdbdc242ca793f350c71941cfa361d7f6
El-Leto <elleto.life@gmail.com>
2021-08-19 17:48:58 +0300

    Add WhatsApp button

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -1,9 +1,11 @@
 $(window).scroll(function() {
   var height = $(window).scrollTop();
-  if (height > 100) {
+  if (height > 200) {
       $('#back2Top').fadeIn();
+      $('.whatsapp-btn').fadeIn();
   } else {
       $('#back2Top').fadeOut();
+      $('.whatsapp-btn').fadeOut();
   }
 });
 $(document).ready(function() {
