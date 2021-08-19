32169c02903d1352f9b47528aaedf75c34268545
El-Leto <elleto.life@gmail.com>
2021-08-19 13:40:33 +0300

    Add top scroll

--- js/snippets.js
+++ js/snippets.js
@@ -1,8 +1,8 @@
 const firstTime = document.querySelectorAll('.first-time');
-firstTime.forEach(item => item.textContent = 'Пн-Пт: 10:00-20:00')
+firstTime.forEach(item => item.textContent = 'Пн-Пт: 10:00-20:00');
 
 const secondTime = document.querySelectorAll('.second-time');
-secondTime.forEach(item => item.textContent = 'Сб: 10:00-18:00 Вс: выходной')
+secondTime.forEach(item => item.textContent = 'Сб: 10:00-18:00 Вс: выходной');
 
 const weekendTime = document.querySelectorAll('.weekend-time');
-weekendTime.forEach(item => item.textContent = '')
+weekendTime.forEach(item => item.textContent = '');
